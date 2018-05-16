977e5f34a2e3c449253a1480eb905d4d17456572
Asia Stuerznickel <astuerznickel@gmail.com>
2018-05-16 20:24:33 +0100

    yelp mvp no design

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,43 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+
+restaurants_attributes = [
+  {
+    name: 'Fish',
+    address: 'Seaside',
+    phone_number: '1234567',
+    category: 'belgian',
+  },
+  {
+    name: 'Bovine',
+    address: 'Hillside',
+    phone_number: '1234567',
+    category: 'italian',
+  },
+  {
+    name: 'Fowel',
+    address: 'Yard',
+    phone_number: '1234567',
+    category: 'chinese',
+  },
+  {
+    name: 'Swine',
+    address: 'Mud Pit',
+    phone_number: '1234567',
+    category: 'french',
+  },
+{
+    name: 'Insect',
+    address: 'Insect Farm',
+    phone_number: '1234567',
+    category: 'japanese',
+  }
+]
+
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
